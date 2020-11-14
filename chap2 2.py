199992c9fedec8bd52da83f9dc185bb4d4469816
disha shetty <50168017+dishashetty5@users.noreply.github.com>
2020-11-14 12:45:16 +0530

    Add files via upload

---
+++ chap2 2.py
@@ -0,0 +1,8 @@
+"""Write a program to prompt the user for hours and rate per
+hour to compute gross pay."""
+hour=input("Enter the hour: ")
+h=float(hour)
+rate=input("enter the rate: ")
+r=float(rate)
+pay=(h*r)
+print(pay)
