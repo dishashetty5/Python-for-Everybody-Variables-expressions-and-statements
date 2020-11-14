245bb85d4a1acbcfdb00cee1f642308000845a90
disha shetty <50168017+dishashetty5@users.noreply.github.com>
2020-11-14 12:45:52 +0530

    Add files via upload

---
+++ chap2 5.py
@@ -0,0 +1,6 @@
+""" Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the
+converted temperature"""
+celsius=input("enter the degree in celsius")
+h=float(celsius)
+f=(h*9/5)+32
+print("degree in fhrenheit",f)
